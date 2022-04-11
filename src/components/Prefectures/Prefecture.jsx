bf23ccfdbb7f0e6907239b7643ea6966bf082c7a
nisi0929 <kengo279@icloud.com>
2022-04-11 17:57:13 +0900

    delete checkbox component

--- src/components/Prefectures/Prefecture.jsx
+++ src/components/Prefectures/Prefecture.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Checkbox } from "./Checkbox";
 export const Prefectures = ({
   checkList,
   setCheckList,
@@ -22,6 +21,37 @@ export const Prefectures = ({
       });
   }, []);
 
+  //checkListにcheckした要素を追加
+  const onChange = (e) => {
+    // 取得した都道府県のnum
+    const Nums = checkList.map((n) => {
+      return n.prefCode;
+    });
+
+    if (Nums.includes(e.target.id)) {
+      // すでにある場合(その番号を含むcheckListの削除)
+      setCheckList(
+        checkList.filter((f) => {
+          return f.prefCode !== e.target.id;
+        })
+      );
+      //すでにある場合(その番号を含むpopulationの削除)
+      let tmp = [];
+      population.map((p) => {
+        if (p.prefCode !== e.target.id) {
+          tmp.push(p);
+        }
+      });
+      setPopulation(tmp);
+    } else {
+      // 番号がない場合（単純な追加）
+      setCheckList([
+        ...checkList,
+        { prefName: e.target.value, prefCode: e.target.id },
+      ]);
+    }
+  };
+
   //   return
   return (
     <div>
@@ -30,13 +60,14 @@ export const Prefectures = ({
           return (
             <div key={p.prefName}>
               <div className="flex">
-                <Checkbox
-                  checkList={checkList}
-                  setCheckList={setCheckList}
-                  prefecture={p}
-                  population={population}
-                  setPopulation={setPopulation}
+                <input
+                  type="checkbox"
+                  value={p.prefName}
+                  id={p.prefCode}
+                  checked={checkList[prefectures]}
+                  onChange={onChange}
                 />
+
                 <p className="pref-text">{p.prefName}</p>
               </div>
             </div>
