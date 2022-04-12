d9d8a67b4b7b8aa5d09dfdfd1265363ef3b41b94
nisi0929 <kengo279@icloud.com>
2022-04-12 18:11:01 +0900

    move onChange

--- src/App.jsx
+++ src/App.jsx
@@ -4,72 +4,15 @@ import { Prefectures } from "./components/Prefectures/Prefecture";
 import { Population } from "./components/Population/Population";
 import { Header } from "./components/layouts/Header";
 import { useState } from "react";
-import axios from "axios";
 function App() {
   // 都道府県の人口を管理するstate
   const [population, setPopulation] = useState([{}]);
-  // populationに入っているprefCodeの一覧
-  const onChange = (e) => {
-    const Nums = population.map((n) => {
-      return n.prefCode;
-    });
-    if (Nums.includes(e.target.id)) {
-      let tmp = [];
-      population.filter((p) => {
-        if (p.prefCode !== e.target.id) {
-          tmp.push(p);
-        }
-      });
-      setPopulation(tmp);
-    } else {
-      const API_KEY = process.env.REACT_APP_RESAS_API;
-      axios
-        .get(
-          `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${e.target.id}`,
-          {
-            headers: { "X-API-KEY": API_KEY },
-          }
-        )
-        .then((res) => {
-          // 人口を配列にしたもの;
-          const populations = res.data.result.data[0].data.map((r) => {
-            return r.value;
-          });
-          // 年を配列にしたもの;
-          const years = res.data.result.data[0].data.map((r) => {
-            return r.year;
-          });
-          setPopulation(
-            [
-              ...population,
-              {
-                prefCode: e.target.id,
-                name: e.target.value,
-                data: populations,
-                years: years,
-              },
-            ].filter(
-              (element, index, self) =>
-                self.findIndex((e) => e.prefCode === element.prefCode) === index
-            )
-          );
-        })
-        .catch((error) => {
-          // 通信エラーが発生したら
-          console.log("通信失敗");
-          console.log(error.status);
-        });
-    }
-  };
+
   return (
     <div className="App">
       <Header />
-      <Prefectures onChange={onChange} />
-      <Population
-        onChange={onChange}
-        population={population}
-        setPopulation={setPopulation}
-      />
+      <Prefectures population={population} setPopulation={setPopulation} />
+      <Population population={population} />
     </div>
   );
 }
--- src/components/Prefectures/Prefecture.jsx
+++ src/components/Prefectures/Prefecture.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-// eslint-disable-next-line react/display-name
-export const Prefectures = ({ population, onChange }) => {
-  console.log("prefecture");
+export const Prefectures = ({ population, setPopulation }) => {
   // 都道府県の情報一覧を管理するstate
   const [prefectures, setPrefectures] = useState([]);
   // 都道府県 APIの取得
@@ -17,6 +15,57 @@ export const Prefectures = ({ population, onChange }) => {
       });
   }, []);
 
+  // checkした時の挙動
+  const onChange = (e) => {
+    // populationに入っているprefCodeの一覧
+    const PrefCodes = population.map((n) => {
+      return n.prefCode;
+    });
+    // checkされた都道府県がPrefcodesに入ってるか入っていないかの条件分岐
+    if (PrefCodes.includes(e.target.id)) {
+      // [すでに入っていた場合]filterしてすでにある都道府県を取り除く
+      let tmp = [];
+      population.filter((p) => {
+        if (p.prefCode !== e.target.id) {
+          tmp.push(p);
+        }
+      });
+      setPopulation(tmp);
+    } else {
+      // [入っていなかった場合]都道府県の人口推移データの追加
+      const API_KEY = process.env.REACT_APP_RESAS_API;
+      axios
+        .get(
+          `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${e.target.id}`,
+          {
+            headers: { "X-API-KEY": API_KEY },
+          }
+        )
+        .then((res) => {
+          // 人口を配列にしたもの;
+          const populations = res.data.result.data[0].data.map((p) => {
+            return p.value;
+          });
+          // 年を配列にしたもの;
+          const years = res.data.result.data[0].data.map((p) => {
+            return p.year;
+          });
+          setPopulation([
+            ...population,
+            {
+              prefCode: e.target.id,
+              name: e.target.value,
+              data: populations,
+              years: years,
+            },
+          ]);
+        })
+        .catch((error) => {
+          console.log("通信失敗");
+          console.log(error.status);
+        });
+    }
+  };
   //   return
   return (
     <div>
@@ -29,7 +78,6 @@ export const Prefectures = ({ population, onChange }) => {
                   type="checkbox"
                   value={p.prefName}
                   id={p.prefCode}
-                  checked={population}
                   onChange={onChange}
                 />
                 <p className="pref-text">{p.prefName}</p>
